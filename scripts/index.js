494d9616f31faad1e4659fc8a0fec1c0b5df2a23
NikZ <gizm55@yandex.ru>
2021-05-01 23:48:49 +0300

    hotfix refactor js

--- scripts/index.js
+++ scripts/index.js
@@ -108,7 +108,8 @@ function createCard(itemData) {  // создание карточки
   });
 
   cardImage.addEventListener('click', function () { // увеличение фотографии карточки
-    popupScalePhoto.classList.add('popup_opened');
+    switchPopup(popupScalePhoto);
+    // popupScalePhoto.classList.add('popup_opened');
     photoPopup.src = itemData.link;
     photoPopup.alt = 'Фото ' + itemData.name;
     captionPopup.textContent = itemData.name;
